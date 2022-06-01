298ef6b02b9dab06ad2901a6763a6ecbf10509f4
Markus Heikkinen <markus.heikkinen@zhinst.com>
2022-06-01 12:37:03 +0200

    docs: Update project documentation
    Co-authored-by: Andrea Nocco <andrea.nocco@zhinst.com>

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+# https://betterprogramming.pub/auto-documenting-a-python-project-using-sphinx-8878f9ddc6e9
 
 # -- Path setup --------------------------------------------------------------
 
@@ -13,25 +14,27 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../zhinst/hdiq"))
+sys.path.insert(0, os.path.abspath("../zhinst/hdiq"))
+print(f"Current working directory: {os.getcwd()}")
 
 
 # -- Project information -----------------------------------------------------
 
 project = "zhinst-hdiq"
-copyright = "2020, zi"
+copyright = "2020, Zurich Instruments AG"
 author = "Zurich Instruments"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.3.0"
+from importlib.metadata import version as version_
 
+release = version_("zhinst.hdiq")
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "m2r2"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -47,9 +50,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ['_static']
+html_static_path = []
