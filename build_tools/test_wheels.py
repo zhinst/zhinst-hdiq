f78661fd224c4952e48cb73ca17053b42b099042
Markus Heikkinen <markus.heikkinen@zhinst.com>
2023-03-20 13:15:00 +0100

    Fix wheels test

--- build_tools/test_wheels.py
+++ build_tools/test_wheels.py
@@ -8,7 +8,7 @@ def test_source_distribution_exists(version: str, dist_dir="dist"):
     print(os.listdir(dist_dir))
     assert set(os.listdir(dist_dir)) == set([
         f"zhinst_hdiq-{version}-py3-none-any.whl",
-        f"zhinst-hdiq-{version}.tar.gz"
+        f"zhinst_hdiq-{version}.tar.gz"
     ])
 
 if __name__ == "__main__":
